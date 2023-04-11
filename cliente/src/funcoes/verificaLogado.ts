c2c28ce22b02efde92f889f48b354e09c0df469e
Bruno Denardo <brunoden.bd@gmail.com>
2023-04-11 18:22:10 -0300

    Função verificaLogado agora lê um dos elementos do objeto que o backend manda

--- cliente/src/funcoes/verificaLogado.ts
+++ cliente/src/funcoes/verificaLogado.ts
@@ -3,12 +3,10 @@ import api from "../services/api"
 export default async function verificaLogado(){
     const login = await api.get('/confereLogado')
     console.log(`Session: ${login.data}`)
-    if (login.data){
-        console.log(login.data)
+    if (login.data.logado){
         return true
     }
     else{
-        console.log(login.data)
         return false
     }
       
