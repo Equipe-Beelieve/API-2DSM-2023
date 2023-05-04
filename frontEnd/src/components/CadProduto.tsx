48ca85d153571dc9d31fcf72a3ce1f6e75535ab8
Bruno Denardo <brunoden.bd@gmail.com>
2023-05-04 16:40:46 -0300

    Funções que mudam os valores dos campos tipo, regra e obrigatoriedade na tela de cadastro de usuário

--- frontEnd/src/components/CadProduto.tsx
+++ frontEnd/src/components/CadProduto.tsx
@@ -4,13 +4,52 @@ import NavBar from './NavBar'
 import { Link, redirect, useNavigate } from 'react-router-dom'
 import verificaLogado from '../funcoes/verificaLogado'
 
+interface Regra{
+    tipo:string,
+    valor:string,
+    obrigatoriedade:boolean
+}
+
 function CadProduto() {
 
     const [descricao, setDescricao] = useState('')
     const [unidadeMedida, setUnidadeMedida] = useState('')
-
+    const [regras, setRegras] = useState<Regra[]>([{tipo:'Mínimo de conformidade', valor:'', obrigatoriedade:true}, {tipo:'Avaria', valor:'Não deve haver', obrigatoriedade:true}])
+    const [render, setRender] = useState(0)
 
     const navigate = useNavigate()
+
+
+    function mudaTipo(e:any, id:number){
+        let regra = regras
+        regra[id].tipo = e.target.value
+        setRegras(regra)
+        setRender(render+1)
+        console.log(regras[id].tipo)
+    }
+
+    function mudaParametro(e:any, id:number){
+        let regra = regras
+        regra[id].valor = e.target.value
+        setRegras(regra)
+        setRender(render+1)
+        console.log(regras[id].valor)
+    }
+
+    function mudaObrigatoriedade(e:any, id:number){
+        let regra = regras
+        console.log(e.target.checked)
+        if(e.target.checked === true){
+            regra[id].obrigatoriedade = true
+        }
+        else{
+            regra[id].obrigatoriedade = false
+        }
+        setRegras(regra)
+        setRender(render+1)
+        console.log(regras[id].obrigatoriedade)
+    }
+
 
     useEffect(() => {
         async function veLogado() {
@@ -24,7 +63,7 @@ function CadProduto() {
             }
         }
         veLogado()
-    }, [])
+    }, [render])
 
     async function cadastroProduto(evento: any) {
         evento.preventDefault()
@@ -41,7 +80,7 @@ function CadProduto() {
         <>
             <NavBar />
             <div className='divFornecedor'>
-                <form onSubmit={cadastroProduto}>
+                <form>
                     <h1 className='mainTitle'>Cadastro de Produtos</h1>
 
                     <div className="grid-container poscentralized">
@@ -94,32 +133,58 @@ function CadProduto() {
                     <div className='box3'>
                         <h1 className='mainTitle'>Regras de Recebimento</h1>
                         <br /><br />
-                        <div className='minimo'>
-                            <label>Tipo de Regra:</label>
-                            <select className='input_form' name="unidadeMedida" id="unidadeMedida" disabled>
-                                <option value=""></option>
-                                <option value="minimo" selected>Minimo de <br /> Conformidade</option>
-                            </select>
+            
+                        {regras.map((valor, id) =>
+                            <>
+                            {id === 0 && 
+                                <div className='minimo'>
+                                    <label>Tipo de Regra:</label>
+                                    <select className='input_form' name="unidadeMedida" id="unidadeMedida" disabled>
+                                        <option value=""></option>
+                                        <option value="minimo" selected>Minimo de <br /> Conformidade</option>
+                                    </select>
 
-                            <label>Limitação:</label>
-                            <span>2 regras</span>
-                            <label>Obrigatória:</label>
-                            <input type="checkbox" className='checkbox' />
-                            <br /><br />
-                        </div>
-                        <div className='avaria'>
-                            <label>Tipo de Regra:</label>
-                            <select className='input_form' name="unidadeMedida" id="unidadeMedida" disabled>
-                                <option value=""></option>
-                                <option value="avaria" selected>Avarias</option>
-                            </select>
-                            <label>Limitação:</label>
-                            <span>Não deve Haver</span>
-                            <label>Obrigatória:</label>
-                            <input type="checkbox" className='checkbox' />
-                            <br /><br />
-                        </div>
+                                    <label>Limitação:</label>
+                                    <span>2 regras</span>
+                                    <label>Obrigatória:</label>
+                                    <input type="checkbox" className='checkbox' />
+                                    <br /><br />
+                                </div> 
+                            }
+                            {id === 1 && 
+                                <div className='avaria'>
+                                    <label>Tipo de Regra:</label>
+                                    <select className='input_form' name="unidadeMedida" id="unidadeMedida" disabled>
+                                        <option value=""></option>
+                                        <option value="avaria" selected>Avarias</option>
+                                    </select>
+                                    <label>Limitação:</label>
+                                    <span>Não deve Haver</span>
+                                    <label>Obrigatória:</label>
+                                    <input type="checkbox" className='checkbox' />
+                                    <br /><br />
+                                </div>
+                            }
+                            {id > 1 &&
+                                <div className='avaria'>
+                                    <label>Tipo:</label>
+                                    <select className='input_form' value={regras[id].tipo} onChange={(e) => {mudaTipo(e, id)}}>
+                                        <option value=""></option>
+                                        <option value="Umidade" selected>Umidade</option>
+                                        <option value="Pureza">Pureza</option>
+                                        <option value="Personalizada">Personalizada</option>
+                                    </select>
 
+                                    <label>Regra:</label>
+                                    <input type='text' className='input_form' value={regras[id].valor} onChange={(e) => {mudaParametro(e, id)}}/>
+
+                                    <label>Obrigatoriedade:</label>
+                                    <input type="checkbox" checked={regras[id].obrigatoriedade} onChange={(e) => {mudaObrigatoriedade(e, id)}}/>
+                                </div>
+                            }
+                            </>  
+                        )}
+        
                         <a href="">Adicionar mais regras</a>
                     </div>
                     <div className="grid-container poscentralized">
