ed2f934afd0a129a4f4a4aa4459ae8caca7d67d5
unknown <matheusfernando.vm@gmail.com>
2023-05-13 13:52:51 -0300

    Corrigindo alguns problemas de estilização do componente de Análise Quantitativa

--- frontEnd/src/components/AnaliseQuant.tsx
+++ frontEnd/src/components/AnaliseQuant.tsx
@@ -182,20 +182,19 @@ function AnaliseQuant(){
                 {mudanca === 'Primeira vez' &&
                     <>
                     <div className='mesmalinha'>
+                        <button type="button" onClick={cancelaVoltaListagem} className="cancel_button">Cancelar</button>
                         <button type="button" onClick={confirmaContinua} className="confirm_button">Confirmar</button>
                     </div>
-                    <button type="button" onClick={cancelaVoltaListagem} className="cancel_button">Cancelar</button>
                     </>
                 }
                 {mudanca === 'Edição' &&
                     <>
                     <div className='mesmalinha'>
+                        <button type="button" onClick={cancelaVoltaListagem} className="cancel_button">Cancelar</button>
                         <button type="button" onClick={editaVolta} className="confirm_button">Editar</button>
                     </div>
-                    <button type="button" onClick={cancelaVoltaListagem} className="cancel_button">Cancelar</button>
                     </>
-                }
-                
+                }                
             </div>
             </>
         )
